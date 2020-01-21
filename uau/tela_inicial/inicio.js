b5cb84c2218613cde5be6c26efa32d9d4544bbd2
Juliana Yuki Okubo <juliana.okubo@watermelontecnologia.com>
2020-01-21 16:39:34 -0300

    Comecando gerador nome de sinuca

--- uau/tela_inicial/inicio.js
+++ uau/tela_inicial/inicio.js
@@ -1,35 +1,31 @@
 import React, {useEffect, useState} from 'react'
 
 import {
-  SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Image, TextInput, Button, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback
+  StyleSheet, ScrollView, View, Text, StatusBar, Image, TextInput, Button, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback,
+  FlatList
 } from 'react-native';
 
-const src = require('../images/night-sky.jpg');
+const src = require('../images/nebula.png');
+
+const headerColor = "#01172f";
 
 const styles = StyleSheet.create({
   image: {
-    resizeMode: "center",
-    width: "50%",
-    height: 50,
-    backgroundColor: "skyblue",
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "black",
+    flex: 1,
+    position: "absolute",
   },
   baseScreen: {
     flex: 1,
     flexDirection: "column",
   },
-  textInput: {
-    height: 50,
-    backgroundColor: "white",
-    color: "steelblue",
-  },
-  container: {
-    padding: 10,
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "pink",
-    width: 100,
-    height: 50,
+  title: {
+    fontSize: 40,
+    color: "white",
+    backgroundColor: headerColor,
   }
 });
 
@@ -43,35 +39,13 @@ function Oi(props){
 
 function Main(props) {
 
-  function handleClick(){
-    alert("clicked")
-  }
-
   const [bla, setBla] = useState('');
   return(
     <View style={styles.baseScreen}>
-      <Oi text={"oi"} />
       <Image source={src} style={styles.image}/>
-      <View style={{backgroundColor: "steelblue", width: "100%", padding: 10, height: "auto"}}>
-        <TextInput style={styles.textInput} onChangeText={(text) => setBla(text)} value={bla}/>
-        <Text style={{padding: 10, fontSize: 40, color: "white", width: "auto", height: "auto"}}>
-          {
-            bla.split(' ').map((word) => {
-              return (
-                word && 'bla'
-              )
-            }).join(' ')
-          }
-        </Text>
-      </View>
-      <Button onPress={() => handleClick()} color="#841584" title="test"/>
-      <View style={styles.container}>
-        <TouchableHighlight  onPress={() => handleClick()} underlayColor="white">
-          <View style={styles.button}></View>
-        </TouchableHighlight>
-        <TouchableNativeFeedback onPress={() => handleClick()}>
-          <View style={styles.button}></View>
-        </TouchableNativeFeedback>
+      <StatusBar backgroundColor={headerColor}/>
+      <View style={{flex: 1}}>
+        <Text style={styles.title}>Gerador de apelido para sinuca</Text>
       </View>
     </View>
   )
